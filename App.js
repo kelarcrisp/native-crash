68dfa6fe1b402ee621a4b9f5c957b3425a1e9036
Student <example@example.com>
2019-10-01 14:26:46 -0700

    added a flatlist for scrolling and more comments about how things are working

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -10,22 +10,27 @@ export default function App() {
   };
   //this is veryifying we get the latest state we have with currentGoals and updating that to enteredGoal
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+    setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: enteredGoal }])
   };
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Course Goal' style={styles.textInput} onChangeText={goalInputHandler}
+        <TextInput placeholder='Course Goal'
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
           value={enteredGoal}
         />
         <Button title='ADD' onPress={addGoalHandler} />
       </View>
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData =>
+          <View style={styles.listItem}>
+            <Text> {itemData.item.value}</Text>
+          </View>} />
 
-      <View>
 
-        {courseGoals.map((goal) => <Text>{goal}</Text>)}
-      </View>
     </View>
   );
 }
@@ -44,15 +49,28 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+
+
   }
 });
 
 //text has to be within <Text> tags
       // most tags support the style property as seen in the view tag above. When you are using an inline style as we are above it comes from an object. The other option is to use a stylesheet object(preferred)-- this also comes with built in validation
 
-      //React Native uses flex box to style, the default is to style in columns but you can change with flerxdirection: 'row', flex properties are added to a property inside of a flexbox. aka the child.that can be a view or anything else. this 'flex' property takes a number.---FLEX BOX IS V IMPORTANT---
+        //React Native uses flex box to style, the default is to style in columns but you can change with flerxdirection: 'row', flex properties are added to a property inside of a flexbox. aka the child.that can be a view or anything else. this 'flex' property takes a number.---FLEX BOX IS V IMPORTANT--- 
 
-//React hooks-- useState() returns an array. this array ALWAYS contains 2 elements, the current state and the 2nd is a function that will override our current state. these two elements are ALWAYS returned in a destructured array with usestate({})
+        //anytime you want the page to have a scroll view you have to import ScrollView and replace your View with ScrollView, you can wrap this for the whole page or just the part that you want to be scrollable
+
+        //React hooks-- useState() returns an array. this array ALWAYS contains 2 elements, the current state and the 2nd is a function that will override our current state. these two elements are ALWAYS returned in a destructured array with usestate({})
         //when you use this fucntion to change the state it overrides EVERYTHING in that state EVEN if you only change one thing, the others will now be non existant. so in the case of only wanting to change one part of the state use the ... spread operator, then change the state. state doesnt have to be an object it can be anytning you need it to be
+        //anytime youre looping through an element make sure to assign it a key
 
         //useEffect() takes place of componenet did mount, useEffect takes a function as an argument. 
+
